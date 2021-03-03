2871dc863cf20e3b00e72f47a595b4f25ccb1bed
Heygon Lago <dryvos@gmail.com>
2021-03-03 12:00:55 -0300

    Seed automatizado

--- backend/database/seed/seed.js
+++ backend/database/seed/seed.js
@@ -2,6 +2,7 @@ const crypto        = require('crypto');
 const mongoose      = require("mongoose");
 const requireDir    = require('require-dir');
 const env           = require('dotenv/config');
+const fetch         = require('node-fetch');
 
 
 //Carrega todos os models e deixa úblico
@@ -19,70 +20,28 @@ mongoose
   })
   .then(async () => {
 
-    try {
-        await Usern.create({
-            "gender": "1",
-            "name": {
-            "title": "1",
-            "first": "1",
-            "last": "1"
-            },
-            "location": {
-            "street": {
-                "number": "1",
-                "name": "1"
-            },
-            "city": "1",
-            "state": "1",
-            "postcode": "1",
-            "coordinates": {
-                "latitude": "1",
-                "longitude": "1"
-            },
-            "timezone": {
-                "offset": "1",
-                "description": "1"
-            }
-            },
-            "email": "1",
-            "login": {
-            "uuid": "1",
-            "username": "1",
-            "password": "1",
-            "salt": "1",
-            "md5": "1",
-            "sha1": "1",
-            "sha256": "1"
-            },
-            "dob": {
-            "date": "1",
-            "age": "1"
-            },
-            "registered": {
-            "date": "1",
-            "age": "1"
-            },
-            "phone": "1",
-            "cell": "1",
-            "userId": {
-            "name": "1",
-            "value": "1"
-            },
-            "picture": {
-            "large": "1",
-            "medium": "1",
-            "thumbnail": "1"
-            },
-            "nat": "1"
-        });
 
-        console.log('Depois do salvar');
+    fetch('https://randomuser.me/api/?results=500', {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json())
+    .then(async json => {
+
+        for (let u = 0; u <= json.results.length; u++) {
+            try {
+
+                await Usern.create(json.results[u]);
+                
+            } catch (error) {
+                console.log('Erro ao salvar');
+                return false;
+            }   
+        }
+
+        console.log('Seed concluído');
         return true;
-        
-    } catch (error) {
-        console.log('Erro ao salvar');
-        return false;
-    }
 
+    });
 
   });
